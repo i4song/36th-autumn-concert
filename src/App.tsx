92384279fd2cd14a71926bedaa537ebb79291a8b
yongwookLee <yongw00k@g.skku.edu>
2022-08-01 15:09:03 +0900

    :pencil2: Change directory name

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CustomAudio from './components/CustomAudio';
-import Gradient from './components/Gradient';
+import Gradient from './components/GradientAnimate';
 
 const App = () => {
   return (
